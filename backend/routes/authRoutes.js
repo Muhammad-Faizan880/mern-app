532c0792e1cc882a93144f5a3c857af7a9e11577
Muhammad-Faizan880 <faizan@codendcoffee.com>
2026-04-17 14:49:31 +0500

    revert the logouty route later

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,5 +7,7 @@ router.post("/register", register);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/logout1", logout);
+
 
 export default router;
